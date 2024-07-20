053b8429f46796e27d08b15729048530c3755d81
Michael Alejandro <maalejandro@puce.edu.ec>
2024-07-20 10:54:04 -0500

    conexion postgrest y formulario

--- lab8/settings.py
+++ lab8/settings.py
@@ -76,18 +76,19 @@ WSGI_APPLICATION = 'lab8.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pokedex_michael',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'pucetec'
     }#,
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'pokedex',
-#        'HOST': 'localhost',
-#        'PORT': '5432',
-#        'USER': 'postgres',
-#        'PASSWORD': 'postgres'
+    #    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 }
+
 
 
 # Password validation
@@ -125,6 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#Media files
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from django.template import loader
+from pokedex.forms import PokemonForm
 from .models import Pokemon
 
 def index(request):
@@ -14,3 +16,14 @@ def pokemon(request, pokemon_id):
         'pokemon': pokemon
     }
     return HttpResponse(template.render(context, request))
+
+def add_pokemon(request):
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm()
+    
+    return render(request, 'add_pokemon.html', {'form': form})
