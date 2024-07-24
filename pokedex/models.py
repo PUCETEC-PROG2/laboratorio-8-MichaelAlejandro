2bbcead3ce1e098f2da0c78de694bee1556fa140
Michael Alejandro <maalejandro@puce.edu.ec>
2024-07-24 17:46:33 -0500

    Crear pagina de login

--- pokedex/models.py
+++ pokedex/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Trainer (models.Model):
     first_name = models.CharField(max_length=30, null=False)
     last_name = models.CharField(max_length=30, null=False)
-    birth_dtae = models.DateField(null=False)
+    birth_date = models.DateField(null=False)
     level = models.IntegerField(default=1)
     
     def __str__(self) -> str:
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.template import loader
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 from pokedex.forms import PokemonForm
 from .models import Pokemon
 
@@ -17,6 +19,7 @@ def pokemon(request, pokemon_id):
     }
     return HttpResponse(template.render(context, request))
 
+@login_required
 def add_pokemon(request):
     if request.method == 'POST':
         form = PokemonForm(request.POST, request.FILES)
@@ -27,3 +30,7 @@ def add_pokemon(request):
         form = PokemonForm()
     
     return render(request, 'add_pokemon.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    
